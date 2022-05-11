b218cfe96262acfdb342c9d48ba7f0fc3af82b4d
SafirEscutia <89230203+SafirEscutia@users.noreply.github.com>
2022-05-10 23:42:11 -0500

    Ejemplos del Live 3 de la semana 1

---
+++ weekly_mission_1/example_0/main.js
@@ -0,0 +1,17 @@
+console.log("Hola LaunchX desde Node JS")
+
+let myCar = new Object();
+myCar.model = "Audi"
+
+const myModule = (() => {
+    const privateFoo = "No quiero que vean esto"
+
+    const exported = {
+        publicFoo: "Quiero que vean esto"
+    }
+
+    return exported
+})
+
+console.log(myModule.privateFoo)
+console.log (myModule.publicFoo)
